dd5308825468f64be7816fa8589b2a73e39df7a4
Jonathan <jonathan.ohkubo@gmail.com>
2020-09-26 19:20:25 -0500

    clarified that users cannot do query for all cities.

--- bikeshare.py
+++ bikeshare.py
@@ -19,7 +19,7 @@ def get_filters():
     (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # get user input for city (chicago, new york city, washington). In current set-up, user cannot select 'all' cities. HINT: Use a while loop to handle invalid inputs
     try:
         city = input('Please enter a city: chicago, new york city or washington.').lower()       
         while (city not in city_names):
