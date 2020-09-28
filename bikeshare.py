fe6108115319178cecdf7fead0e31116b6c9f95f
Jonathan <jonathan.ohkubo@gmail.com>
2020-09-27 22:46:52 -0500

    refactor: clarified in python.py that Washington has no gender data.

--- bikeshare.py
+++ bikeshare.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 
 #Washington has no age data.
+#Washington has no gender data.
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
